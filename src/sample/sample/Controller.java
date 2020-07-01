5e7dcdb82921945a4de2c8a48af79763f7a95baf
BitoAti <bitoati1987@gmail.com>
2020-07-01 11:09:07 +0200

    actionButtons and step function

--- src/sample/sample/Controller.java
+++ src/sample/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import sample.game.Game;
 
 import javax.swing.text.html.HTML;
@@ -17,14 +18,38 @@ public class Controller {
 
 
     @FXML
-    public Label mainPane;
-
+    public AnchorPane mainPane;
+    public Button startButton;
+    public AnchorPane gamePane;
+    public AnchorPane menuPane;
 
     @FXML
     public void startGame(ActionEvent actionEvent){
         Game game = new Game();
         game.start();
-        mainPane.setVisible(false);
+        menuPane.setVisible(false);
+        gamePane.setVisible(true);
+
+    }
+
+
+
+
+    @FXML
+    public void eat(){
+        System.out.println("eat");
+    }
+    @FXML
+    public void sleep(){
+        System.out.println("sleep");
+    }
+    @FXML
+    public void learn(){
+        System.out.println("learn");
+    }
+    @FXML
+    public void play(){
+        System.out.println("play");
     }
 
 
